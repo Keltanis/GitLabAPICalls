85395cb84ae6f4df47d0b791bf9f81ec30ad8d0c
Keltanis <42776871+Keltanis@users.noreply.github.com>
2026-09-24 21:33:38 -0500

    Create Get Issue Schema.ps1

---
+++ ps/Get Issue Schema.ps1
@@ -0,0 +1,16 @@
+$baseUrl   = "https://gitlab.com"
+$projectId = <PROJECT ID>
+$token     = "<GAT>"
+
+$uri = "$baseUrl/api/v4/projects/$projectId/issues?state=all&per_page=1"
+
+$issues = Invoke-RestMethod -Method Get -Uri $uri -Headers @{
+    "PRIVATE-TOKEN" = $token
+}
+
+if ($issues.Count -eq 0) {
+    Write-Host "No issues found in this project."
+}
+else {
+    $issues[0].PSObject.Properties.Name | Sort-Object
+}
